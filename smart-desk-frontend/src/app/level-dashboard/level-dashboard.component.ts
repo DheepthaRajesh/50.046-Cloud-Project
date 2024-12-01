8425679651dc354cd7b002b6a58c527ebb92f5e5
Dheeptha Rajesh <dheeptharajesh@Dheepthas-MacBook-Pro.local>
2024-12-01 17:32:31 +0800

    Update styling for desk dashboard page

--- smart-desk-frontend/src/app/level-dashboard/level-dashboard.component.ts
+++ smart-desk-frontend/src/app/level-dashboard/level-dashboard.component.ts
@@ -10,11 +10,19 @@ import { CommonModule } from '@angular/common';
   styleUrl: './level-dashboard.component.css'
 })
 export class LevelDashboardComponent implements OnInit{
-  level: string = ''; // Stores the current level (e.g., 1 or 2)
+  level: string = ''; // Stores the current formatted level (e.g., Level 1 or Level 2)
+  retrievedlevel: string = ''; // Stores the retrieved level (e.g., level1 or level2)
   desks = [
     { id: 1, available: true },
     { id: 2, available: false },
     { id: 3, available: true },
+    { id: 4, available: true },
+    { id: 5, available: false },
+    { id: 6, available: true },
+    { id: 7, available: true },
+    { id: 8, available: true },
+    { id: 9, available: false },
+    { id: 10, available: true },
     // Add more desks as needed
   ];
 
@@ -22,7 +30,12 @@ export class LevelDashboardComponent implements OnInit{
 
   ngOnInit() {
     // Fetch the 'level' from the route parameters
-    this.level = this.route.snapshot.paramMap.get('level') || '1';
+    this.retrievedlevel = this.route.snapshot.paramMap.get('level') || 'Level 1';
+
+    // Format the level and display in desired format:
+    this.level =
+      this.retrievedlevel.charAt(0).toUpperCase() +
+      this.retrievedlevel.slice(1).replace(/\d+/, ' $&');
   }
 
   goToDeskTrends(deskId: number) {
